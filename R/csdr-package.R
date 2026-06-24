960ba7ff1f020d0bff84a0b077a4c8be0b496bef
tXiao95 <tXiao95@gmail.com>
2026-06-24 01:32:47 -0400

    Document primary csdr APIs

--- R/csdr-package.R
+++ R/csdr-package.R
@@ -1,3 +1,14 @@
+#' Causal sufficient dimension reduction
+#'
+#' The package entry point is [csdr()], which constructs causal SDR targets and
+#' fits MAVE for one or more variants. Advanced users can call [csdr_target()]
+#' for target construction only, [estimate_ers()] for exposure-response surface
+#' estimation, [estimate_pseudo_outcomes()] for pseudo-outcome construction,
+#' and [estimate_residualized_pair()] for residualized-pair construction.
+#'
+#' Learners are specified with [csdr_learners()], [sl_regression()],
+#' [mvn_gps()], [custom_regression()], and [custom_gps()].
+#'
 #' @keywords internal
 "_PACKAGE"
 
